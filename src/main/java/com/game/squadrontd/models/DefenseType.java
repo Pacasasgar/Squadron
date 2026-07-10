6bde5d58e3f830120754d9c70db1f5c146c5a2ea
Pau Casas <pacasasgar@gmail.com>
2026-07-10 12:01:48 +0200

    Cambios de balance

--- src/main/java/com/game/squadrontd/models/DefenseType.java
+++ src/main/java/com/game/squadrontd/models/DefenseType.java
@@ -4,10 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum DefenseType {
-    INFANTRY(10, 50, 10, DamageType.PHYSICAL, ArmorType.LIGHT),  // cost, hp, attack
-    ARCHER(15, 20, 25, DamageType.PHYSICAL, ArmorType.LIGHT),
-    KNIGHT(25, 150, 35, DamageType.PHYSICAL, ArmorType.HEAVY),
-    MAGE(20, 40, 30, DamageType.MAGIC, ArmorType.LIGHT);
+    INFANTRY(10, 60, 10, DamageType.PHYSICAL, ArmorType.LIGHT),  // cost, hp, attack
+    ARCHER(15, 30, 35, DamageType.PHYSICAL, ArmorType.LIGHT),
+    KNIGHT(25, 120, 20, DamageType.PHYSICAL, ArmorType.HEAVY),
+    MAGE(20, 40, 40, DamageType.MAGIC, ArmorType.LIGHT);
 
     private final int cost;
     private final int hp;
